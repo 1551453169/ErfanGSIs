8d8f41d0b2bdb20862fd3626d9fbe26080c930f8
1551453169 <1551453169@qq.com>
2020-06-18 16:59:32 +0800

    Update and rename roms/9/Flyme/debloat.sh to 光盘/9/带我飞/debloat.sh

---
+++ 光盘/9/带我飞/debloat.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+systempath=$1
+thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
+
+rm -rf $1/recovery-from-boot.p
+# We have chrome for webview. so 100mb less size
+rm -rf $1/app/MzUpdate*
